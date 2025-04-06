5952061142954e07c3285599a1b5f2d20e7836dc
mfardin <mfardin@student.ubc.ca>
2025-04-05 21:03:43 -0700

    23: Stretch Goal: make timeline better
    - Converted bar chart to area graph
    - Updated formatting for tooltips (added commas)
    - Added line indicator for timeline

--- js/BarChart.js
+++ js/BarChart.js
@@ -1,10 +1,12 @@
-class BarChart {
+class TimeLine {
   constructor(_config, _data, _dispatcher) {
     this.config = {
       parentElement: _config.parentElement,
       containerWidth: _config.containerWidth || 1250,
       containerHeight: _config.containerHeight || 140,
-      margin: _config.margin || { top: 10, right: 5, bottom: 50, left: 5 }
+      margin: _config.margin || { top: 10, right: 5, bottom: 50, left: 50 },
+      tooltipPadding: 10,
+      gradientColours: ['#0e1031', '#fdf6c1']
     };
     this.data = _data;
     this.dispatcher = _dispatcher;
@@ -27,37 +29,70 @@ class BarChart {
     vis.chart = vis.svg.append('g')
       .attr('transform', `translate(${vis.config.margin.left}, ${vis.config.margin.top})`);
 
-    // border box around the chart
-    vis.chart.append('rect')
-      .attr('x', 0)
-      .attr('y', 0)
-      .attr('width', vis.width)
-      .attr('height', vis.height)
-      .attr('fill', 'none')
-      .attr('stroke', 'black');
-
-        vis.xScale = d3.scaleTime()
-            .range([0, vis.width]);
-
-    vis.yScale = d3.scaleLinear()
-      .range([vis.height, 10])
-      // .domain([0, vis.getMax()]);
-
-
+    vis.xScale = d3.scaleTime()
+      .range([0, vis.width]);
+    
+    vis.xAxis = d3.axisBottom(vis.xScale)
+      .tickFormat(d3.timeFormat('%b'))
+      .tickSize(0)
+      .tickSizeOuter(0)
+      .tickPadding(7);
+    
     vis.xAxisGroup = vis.chart.append('g')
+      .attr('class', 'axis x-axis')
       .attr('transform', `translate(0, ${vis.height})`);
 
-    vis.yAxisGroup = vis.chart.append('g');
+    vis.yScale = d3.scaleLinear().range([vis.height, 5]);
+    
+    vis.yAxis = d3.axisLeft(vis.yScale)
+      .tickSize(-vis.width)
+      .tickFormat((d, i, ticks) => (i === 0 || i === ticks.length - 1) ? d : '')
+      .tickSizeOuter(0);
 
-    // Tooltip setup
-    vis.tooltip = d3.select("body").append("div")
-      .style("position", "absolute")
-      .style("background", "white")
-      .style("border", "1px solid black")
-      .style("padding", "5px")
-      .style("font-size", "12px")
-      .style("display", "none");
+    vis.yAxisGroup = vis.chart.append('g')
+      .attr('class', 'axis y-axis');
 
+    // Tooltip indicator
+    vis.hoverLine = vis.chart.append('line')
+      .attr('class', 'hover-line')
+      .style('display', 'none');
+
+    // ==========================================
+    // Gradient
+    // ==========================================
+    const colourScale = d3.scaleSymlog()
+      .range(vis.config.gradientColours)
+      .domain([0, 1])
+      .interpolate(d3.interpolateHcl);
+
+    const gradOffsets = d3.range(0, Math.sqrt(1), Math.sqrt(1 / 5))
+      .map(d => d ** 2);
+
+    const gradStops = gradOffsets.map(d => {
+      return {
+        colour: colourScale(d),
+        value: d,
+        offset: d * 100
+      }
+    });
+
+    vis.linearGradient = vis.svg.append('defs').append('linearGradient')
+      .attr('id', 'timeline-gradient')
+      .attr('x1', '0%')
+      .attr('y1', '0%')
+      .attr('x2', '0%')
+      .attr('y2', '100%');
+
+    vis.linearGradient.selectAll('stop')
+        .data(gradStops)
+      .join('stop')
+        .attr('offset', d => `${d.offset}%`)
+        .attr('stop-color', d => d.colour)
+        .attr('stop-opacity', '80%');
+
+    // ==========================================
+    // Brush
+    // ==========================================
     // Add brush
     vis.brush = d3.brushX()
       .extent([[0, 0], [vis.width, vis.height]])
@@ -79,10 +114,15 @@ class BarChart {
     vis.brushGroup = vis.chart.append("g")
       .attr("class", "brush")
       .call(vis.brush);
-    }
+
+    vis.updateVis();
+  }
 
   updateVis() {
     let vis = this;
+
+    vis.xVal = d => d.date;
+    vis.yVal = d => d.total;
 
     const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
@@ -102,104 +142,103 @@ class BarChart {
       return { year, months: monthsArray };
     });
 
-        const allMonths = nestedData.flatMap(d =>
-            d.months.map(m => ({
-                ...m,
-                date: new Date(m.year, m.month - 1)
-            }))
-        );
+    const allMonths = nestedData.flatMap(d =>
+      d.months.map(m => ({
+        ...m,
+        date: new Date(m.year, m.month - 1)
+      }))
+    );
 
-        const extent = d3.extent(allMonths, d => d.date);
-        const lastDate = new Date(extent[1]);
-        lastDate.setMonth(lastDate.getMonth() + 1);
+    const extent = d3.extent(allMonths, d => d.date);
+    const lastDate = new Date(extent[1]);
+    lastDate.setMonth(lastDate.getMonth());
 
-        vis.xScale.domain([extent[0], lastDate]);
+    vis.xScale.domain(extent);
 
     const maxOutage = d3.max(allMonths, d => d.total);
     vis.yScale.domain([0, maxOutage]);
 
     vis.nestedData = nestedData;
+
+    vis.area = d3.area()
+      .x(d => vis.xScale(vis.xVal(d)))
+      .y0(vis.yScale(0))
+      .y1(d => vis.yScale(vis.yVal(d)));
+    
+    vis.bisect = d3.bisector(d => vis.xVal(d)).left;
+
     vis.renderVis();
   }
 
   renderVis() {
     let vis = this;
 
-        const bars = vis.chart.selectAll('.bar')
-            .data(vis.nestedData.flatMap(d => d.months.map(m => ({
-                ...m,
-                date: new Date(m.year, m.month - 1)
-            }))), d => d.date)
-            .join('rect')
-            .attr('class', 'bar')
-            .attr('x', d => vis.xScale(d.date))
-            .attr('width', d => {
-                const nextMonth = new Date(d.year, d.month);
-                const barWidth = vis.xScale(nextMonth) - vis.xScale(d.date);
-                return Math.max(0, barWidth - 8);
-            })
-            .attr('fill', 'steelblue')
-            .on("mouseover", function (event, d) {
-                vis.tooltip
-                    .style("display", "block")
-                    .html(`<strong>${d.monthName} ${d.year}</strong>: ${d.total} outages`);
-            })
-            .on("mousemove", function (event) {
-                vis.tooltip
-                    .style("left", `${event.pageX + 10}px`)
-                    .style("top", `${event.pageY - 20}px`);
-            })
-            .on("mouseleave", function () {
-                vis.tooltip.style("display", "none");
-            });
+    const flatData = vis.nestedData.flatMap(d => d.months.map(m => ({
+      ...m,
+      date: new Date(m.year, m.month - 1)
+    })));
 
-    bars.transition().duration(200)
-      .attr('y', d => vis.yScale(d.total))
-      .attr('height', d => vis.height - vis.yScale(d.total));
+    const chartArea = vis.chart.selectAll('.outage-area')
+        .data([flatData], d => d.date)
+      .join('path')
+        .attr('class', 'outage-area')
+        .on('mousemove', function(event) {
+          const leftOffset = vis.svg.node().getBoundingClientRect().left
+                + vis.config.margin.left
+                + vis.config.tooltipPadding;
 
-        // Year Labels
-        vis.xAxisGroup.selectAll('.year-label')
-            .data(vis.nestedData)
-            .join('text')
-            .attr('class', 'year-label')
-            .attr('x', d => {
-                const firstDate = new Date(d.year, d.months[0].month - 1);
-                const lastDate = new Date(d.year, d.months[d.months.length - 1].month);
-                return (vis.xScale(firstDate) + vis.xScale(lastDate)) / 2;
-            })
-            .attr('y', 35)
-            .attr('text-anchor', 'middle')
-            .text(d => d.year)
-            .attr('fill', 'black')
-            .attr('font-size', '16px');
+          const format = d3.format(",")
 
-        // Month Labels
-        vis.xAxisGroup.selectAll('.month-label')
-            .data(vis.nestedData.flatMap(d => d.months.filter(m => m.month % 3 === 1)))
-            .join('text')
-            .attr('class', 'month-label')
-            .attr('x', d => {
-                const date = new Date(d.year, d.month - 1);
-                const nextMonth = new Date(d.year, d.month);
-                return vis.xScale(date) + (vis.xScale(nextMonth) - vis.xScale(date)) / 2;
-            })
-            .attr('y', 15)
-            .attr('text-anchor', 'middle')
-            .text(d => d.monthName)
-            .attr('fill', 'black')
-            .attr('font-size', '10px');
+          const xPos = d3.pointer(event, this)[0],
+                date = vis.xScale.invert(xPos),
+                i = vis.bisect(flatData, date, 1),
+                d0 = flatData[i - 1],
+                d1 = flatData[i],
+                d = xPos - vis.xScale(d0.date) > vis.xScale(d1.date) - xPos ? d1 : d0;
+  
+          d3.select('#tooltip')
+            .style('display', 'block')
+            .style("left", `${vis.xScale(vis.xVal(d)) + leftOffset}px`)
+            .style("top", `${event.pageY - vis.config.tooltipPadding}px`)
+            .html(`<div class="tooltip-title"><strong>${d.monthName} ${d.year}</strong>: ${format(d.total)} outages</div>`);
 
-    vis.yAxisGroup.selectAll('.tick text').remove();
+          vis.hoverLine
+            .raise()
+            .style('display', 'block')
+            .attr('x1', vis.xScale(vis.xVal(d)))
+            .attr('x2', vis.xScale(vis.xVal(d)))
+            .attr('y1', vis.height)
+            .attr('y2', vis.yScale(vis.yVal(d)));
+        })
+        .on('mouseleave', () => {
+          d3.select('#tooltip').style('display', 'none');
+          vis.hoverLine.style('display', 'none');
+        });
+
+    chartArea
+      .transition().duration(200)
+      .attr('d', vis.area)
+      .attr('fill', 'url(#timeline-gradient)');
+
+    // Year Labels
+    vis.xAxisGroup.selectAll('.year-label')
+        .data(vis.nestedData)
+      .join('text')
+        .attr('class', 'year-label')
+        .attr('x', d => {
+          const firstDate = new Date(d.year, d.months[0].month - 1);
+          const lastDate = new Date(d.year, d.months[d.months.length - 1].month);
+          return (vis.xScale(firstDate) + vis.xScale(lastDate)) / 2;
+        })
+        .attr('y', 35)
+        .attr('text-anchor', 'middle')
+        .text(d => d.year)
+        .attr('fill', 'black')
+        .attr('font-size', '16px');
+
+    vis.xAxisGroup.call(vis.xAxis)
+      .call(g => g.select('.domain').remove());
+    vis.yAxisGroup.call(vis.yAxis)
+      .call(g => g.select('.domain').remove());
   }
-
-  // Helpers
-  getMax() {
-    const nestedData = d3.groups(this.data, d => d.year).map(([year, arr]) => {
-      const byMonth = d3.rollup(arr, v => d3.sum(v, d => d.outage_count), d => d.month);
-      return Array.from({ length: 12 }, (_, i) => byMonth.get(i + 1) || 0);
-    });
-
-    const allMonths = nestedData.flat();
-    return d3.max(allMonths);
-}
 }
